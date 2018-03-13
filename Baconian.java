1c1023ee1840b60eed55e849ce238ed7ba6f6afe
Roman <adam.romanowski93@gmail.com>
2018-03-13 15:07:32 +0100

    handling exceptions in message implemented

--- Baconian.java
+++ Baconian.java
@@ -1,31 +1,53 @@
 public class Baconian{
-    
-    private void decode(String message){
-        System.out.println("decoding " + message + "...");
-        
-        try{
 
-            for (int i=4; i>=0; i--){
+    private static void isValidChar(String str) {
                 
-                int letterIndex = message.offsetByCodePoints(0, i);
-                String letter = message.substring(letterIndex, letterIndex + 1);
-                
-                if (letter.equalsIgnoreCase("A")){
-                    
-                }
-
-                System.out.print(letter);
+        for (char c : str.toUpperCase().toCharArray()){
+            if ( c != 'A' && c != 'B') {
+                System.out.println("Wrong letter(s) in message!");
+                System.exit(0);
             }
         }
-        catch (IndexOutOfBoundsException e){
-            System.out.println("bad input!");
+    }
+
+    private static void isValidLength(String str){
+        
+        if (str.codePointCount(0, str.length()) != 5){
+            System.out.println("Wrong length!");
             System.exit(0);
         }
     }
 
+    private static void isValidInput(String input){
+        isValidChar(input);
+        isValidLength(input);
+    }
+    
+    private static void decode(String message){
+        
+        System.out.println("decoding " + message + "...");
+        int alphabetIndex = 0;
+
+        for (int i=4, j=1; i>=0 && j>=0; i--, j*=2){
+
+            int letterIndex = message.offsetByCodePoints(0, i);
+            String letter = message.substring(letterIndex, letterIndex + 1);
+            
+            if (letter.equalsIgnoreCase("A")){
+                alphabetIndex += 0;
+            }
+            else if (letter.equalsIgnoreCase("B")){
+                alphabetIndex += j;
+            }
+
+        }
+        System.out.println(alphabetIndex);
+    }
+
     public void getAttributes(String displayParameter, String message){
-        if (displayParameter.equalsIgnoreCase("-d")){
-            decode("dupah");
+        if (displayParameter.equalsIgnoreCase("-d")){   
+            isValidInput("aaabb");
+            decode("aaabb");
         }
     }
 }
