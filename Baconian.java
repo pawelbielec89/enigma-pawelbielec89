96b40de9fd44a6020e5ffa040669bb3d62af6d02
Roman <adam.romanowski93@gmail.com>
2018-03-15 13:54:05 +0100

    change alphabet method to public; delete test inputs

--- Baconian.java
+++ Baconian.java
@@ -1,7 +1,7 @@
 public class Baconian{
 
-    private static String[] getAlphabet(){
-        
+    public static String[] getAlphabet(){
+
         String [] alphabet = {"A", "B", "C", "D", "E",
                               "F", "G", "H", "I", "J",
                               "K", "L", "M", "N", "O",
@@ -10,7 +10,7 @@ public class Baconian{
                               "Z"};
         return alphabet;
     }
-    
+        
     private static void decodeChain(String chain){
         
         int alphabetIndex = 0;
@@ -39,7 +39,7 @@ public class Baconian{
         }
     }
 
-    private static int getLetterIndex(String letter){
+    public static int getLetterIndex(String letter){
         
         int letterIndex = 0;
         String[] alphabet = getAlphabet();
@@ -94,11 +94,10 @@ public class Baconian{
     }
 
     public void encription(String mode, String message){
-        // String[] cipher = message.split(" ");
-        String[] cipher = {"aaaaa", "bbaab"};
+        String[] cipher = message.split(" ");
         if (mode.equalsIgnoreCase("-d"))            
             decodeCipher(cipher);
         else
-            encodeCipher("hehu ojre as");
+            encodeCipher(message);
     }
 }
