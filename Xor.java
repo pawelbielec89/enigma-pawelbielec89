968af8a00579f622ba314f35594961e5d6856ac5
Roman <adam.romanowski93@gmail.com>
2018-03-15 14:14:52 +0100

    bug fix; add space encode

--- Xor.java
+++ Xor.java
@@ -2,21 +2,30 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Xor{
-    public static void encript(String message){
+public class Xor {
+    public static void encription(String message) {
 
-      
-        // String message = "UUUU";
+        StringBuilder builder = new StringBuilder();
         String alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int num = 10;
-                
-        for (int i=0; i<message.length();i++){
+
+        for (int i = 0; i < message.length(); i++) {
+            int num = 10;
             String letter = "" + message.charAt(i);
-            int index = Baconian.getLetterIndex(letter);
-            int j = num ^ index;
-            if (j > 25)
-                j = index;
-            System.out.print(alphabetString.charAt(j));
+
+            if (letter.equalsIgnoreCase(" "))
+                builder.append(letter);
+            else {
+                int index = Baconian.getLetterIndex(letter);
+                int j = num ^ index;
+
+                if (j > 25) {
+                    num = 1;
+                    j = num ^ index;
+                }
+                builder.append(alphabetString.charAt(j));
+            }
+
         }
-        }
+        System.out.println(builder.toString());
     }
+}
