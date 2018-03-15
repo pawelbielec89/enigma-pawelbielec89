c3f22273a5920ac96c6904cff9e6ddb0978fc1c8
Roman <adam.romanowski93@gmail.com>
2018-03-15 13:55:54 +0100

    add new cipher

---
+++ Xor.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Xor{
+    public static void main(String[] args){
+
+      
+        String message = "UUUU";
+        String alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int num = 10;
+                
+        for (int i=0; i<message.length();i++){
+            String letter = "" + message.charAt(i);
+            int index = Baconian.getLetterIndex(letter);
+            int j = num ^ index;
+            if (j > 25)
+                j = index;
+            System.out.print(alphabetString.charAt(j));
+        }
+        }
+    }
